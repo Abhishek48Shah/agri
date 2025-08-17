0b5dbbb4f17998825569faacb89f8a9e264b20ba
Abhishek48Shah <shahabhishek8502@gmail.com>
2025-08-17 13:04:09 +0545

    Initial change

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,9 @@ import type { Request, Response, NextFunction } from "express";
 const app = express();
 app.use(express.json());
 const errorHandler = (err, req: Request, res: Response, next: NextFunction) => {
-  if (err) {
+  if (err instanceof ApiError) {
+    ApiError.handel(err);
+  } else {
   }
 };
 app.use(errorHandler);

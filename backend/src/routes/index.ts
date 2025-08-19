df24879d6fe4726db493a115a76d2210eb6cede5
Abhishek48Shah <shahabhishek8502@gmail.com>
2025-08-19 16:25:24 +0545

    add: use instend of get

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
+import { BadRequest } from "../core/apiError";
 import plantAi from "./imageDetection/plantAi";
 const router = express.Router();
 router.use("/plant", plantAi);

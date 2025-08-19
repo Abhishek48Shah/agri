cc7ea900738b0851ff1d548a95163b40134f72ce
Abhishek48Shah <shahabhishek8502@gmail.com>
2025-08-19 16:27:40 +0545

    enhance: routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
+import { ApiError, InternalError } from "./core/apiError";
 import router from "./routes/index";
 const app = express();
 app.use(express.json());
 app.use("/", router);
 const errorHandler = (err, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ApiError) {
-        ApiError.handel(err);
-    } else {
-    }
+  if (err instanceof ApiError) {
+    ApiError.handler(err, res);
+  } else {
+    const internalError = new InternalError(
+      "Internal server error, please try again later",
+    );
+    internalError.send(res);
+  }
 };
 app.use(errorHandler);
 export default app;

1875146a7d0b21d2c0854c35c3391f3bb42b39ce
Abhishek48Shah <shahabhishek8502@gmail.com>
2025-08-19 16:23:07 +0545

    add: Core logic

--- backend/src/routes/imageDetection/plantAi.ts
+++ backend/src/routes/imageDetection/plantAi.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
 import multer from "multer";
-import axios from "axios";
-import { API_KEY } from "../../config";
 import { BadRequest } from "../../helper/apiError";
+import imageService from "../../core/imageService";
+import { SuccessResponse } from "../../core/apiResponse";
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
-router.post("/diagnose", upload.single("image"), async (req, res, next) => {
-  try {
-    new BadRequest("Hello world");
-    console.log(req.file);
-    res.send("file recived");
-  } catch (err: any) {
-    consoel.log(err);
-  }
-});
+router.post(
+  "/diagnose",
+  upload.single("image"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (!req.file) {
+        console.log("No file to show");
+      }
+      const data = await imageService(req.file.buffer);
+      new SuccessResponse(data, "Successfully fetch the data").send(res);
+    } catch (err: any) {
+      return next(err);
+    }
+  },
+);
 export default router;
